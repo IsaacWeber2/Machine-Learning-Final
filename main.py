2372e9255daebd860f4be5c6bc8d84ae0bd45b28
Isaac Weber <isaacweber@Isaacs-Laptop.local>
2025-04-21 13:12:36 -0400

    Added my changes to main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bo
 from sklearn.metrics import rand_score, adjusted_rand_score, mutual_info_score, normalized_mutual_info_score
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
 
 # Loading data and extracting each type
 data = pd.read_csv('dataset.csv')
@@ -42,20 +43,33 @@ train_indices = np.concatenate([brca_train, luad_train, prad_train])
 test_indices = np.concatenate([brca_test, luad_test, prad_test])
 
 
-# Starting with first 3000 features
-reduced_data = data.iloc[:, :3000]
-
-
-#Splitting data
-X_train = reduced_data.loc[train_indices]
-X_test = reduced_data.loc[test_indices]
+X_train_full = data.loc[train_indices]
+X_test_full = data.loc[test_indices]
 y_train = encoded_labels[np.isin(data.index, train_indices)]
 y_test = encoded_labels[np.isin(data.index, test_indices)]
 
 
+#using variance to get best list
+features = 5000  # Number of features to select based on variance
+variances = data.var()
+top_variance_features = variances.nlargest(features).index
+X_train_var = X_train_full[top_variance_features]
+X_test_var = X_test_full[top_variance_features]
+
+#using mutail information to select features
+features2 = 900  # Number of features to select based on mutual information
+mi_selector = SelectKBest(mutual_info_classif, k=features2)
+X_train_selected = mi_selector.fit_transform(X_train_var, y_train)
+X_test_selected = mi_selector.transform(X_test_var)
+
+# Printing both features used
+print(f"Number of features after variance selection: {features}")
+print(f"Number of features after mutual information selection: {features2}")
+
+# Scale the selected features
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_train_scaled = scaler.fit_transform(X_train_selected)
+X_test_scaled = scaler.transform(X_test_selected)
 
 
 knn = KNeighborsClassifier(n_neighbors=5)
